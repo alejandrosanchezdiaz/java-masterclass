f1eb58a5e83df1feee97bcc4ff214d6894d2f92b
Alex <alesandi.uk@gmail.com>
2020-09-23 11:48:43 +0100

    1-commit
    Probando cosas

--- src/Hello.java
+++ src/Hello.java
@@ -1,3 +1,6 @@
 public class Hello {
-    dasdwadsdwad;
+    public static void main(String[] args) {
+        System.out.println("Mi name is Alex");
+        System.out.println("Estoy probando cosas");
+    }
 }
