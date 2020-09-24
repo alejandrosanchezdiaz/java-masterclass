bd5cb08eadc3c4fbcd29e2d98b9c12a2d4ab3824
Alex <alesandi.uk@gmail.com>
2020-09-24 10:18:33 +0100

    2
    2

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Mi name is Alex");
-        System.out.println("Estoy probando cosas");
+        int myFirstNumber = 5;
     }
 }
